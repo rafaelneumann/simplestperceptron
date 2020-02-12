52ec210037af9d50716ff40a9297f87db4b95efd
Rafael Neumann de Oliveira <rafaelneumann@gmail.com>
2020-02-12 15:50:55 -0300

    Created

---
+++ Perceptron.js
@@ -0,0 +1,33 @@
+class Perceptron {
+	
+	constructor() {
+		this.weights = [random(-1,1), random(-1,1)];
+	}
+	
+	guess(inputs) {
+	// inputs should be an array of points with the same length of the weights
+		let sum = 0;
+		
+		for (let i = 0; i < this.weights.length; i++) {
+			sum += this.weights[i] * inputs[i];
+		}
+		
+		return Math.sign(sum);
+	}
+	
+	train(inputs, label) {
+		let my_guess = this.guess(inputs);
+		let error = label - my_guess;
+		
+		for (let i = 0; i < this.weights.length; i++) {
+			this.weights[i] += inputs[i] * error * 0.1;
+		}
+		
+		return [my_guess, error];
+	}
+	
+	print_weights() {
+		console.log(this.weights);
+	}
+	
+}
---
+++ sketch.js
@@ -0,0 +1,73 @@
+class Point {
+	constructor() {
+		this.x = random(0, 1) * side;
+		this.y = random(0, 1) * side;
+		
+		if (this.x > this.y) {
+			this.label = 1;
+		} else {
+			this.label = -1;
+		}
+	}
+	
+	get_inputs() {
+		return [this.x, this.y];
+	}
+	
+	show() {
+		ellipse(this.x, this.y, 15, 15);
+	}
+}
+
+var p;
+var points = [];
+var side = 400;
+
+function setup() {
+	// Initialize the perceptron
+	p = new Perceptron();
+	p.print_weights();
+	
+	// Populate the points
+	for (let i = 0; i < 100; i++) {
+		let pt = new Point;
+		points.push(pt);
+		
+		//console.log(p.guess(pt.get_inputs()));
+	}
+	
+	// Drawing
+	createCanvas(side, side);
+	background(0);	
+}
+
+function draw() {
+	for (let i in points) {
+		stroke(255);
+		strokeWeight(1);
+		
+		// Compare the guess with the label
+		if (p.guess(points[i].get_inputs()) == points[i].label) {
+			fill(0, 255, 0);
+		} else {
+			fill(255, 0, 0);
+		}
+		points[i].show();
+	}
+	
+	// Reference line
+	line(0, 0, side, side);
+}
+
+function mousePressed() {
+	// Train with only one item. Example:
+	
+	for (pt in points) {
+		let item = points[pt];
+		p.train(item.get_inputs(), item.label);
+	}
+	
+	console.log(p.print_weights());
+	
+	
+}
